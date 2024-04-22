7a8e823b12eceecea14ad7282cf972a24b53fa86
Jay Lord Abueva <jaylord.abueva98@gmail.com>
2024-04-22 18:08:22 +0800

    feat: appointments page feat: modal for searching appointment

--- src/components/Appointments/AppointmentsPage.vue
+++ src/components/Appointments/AppointmentsPage.vue
@@ -1,17 +1,70 @@
 <template>
     <div class="flex flex-col justify-center bg-white items-center pl-8 pr-8">
-        <div class="w-full mt-8 flex flex-col">
+        <div class="flex flex-row-reverse w-full">
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                <span>Add New Appointment</span>
+            </button>
         </div>
-    </div>
+        <div class="relative w-full overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                    Your Appointments
+                    <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lorem massa, placerat eu accumsan nec, vestibulum eget metus. Suspendisse mi purus, lacinia non enim nec, consectetur vestibulum sem.
+                    </p>
+                    <SearchButton class="mt-4"/>
+                </caption>
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Patient Name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Schedule
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Status
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Price
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td class="px-6 py-4">
+                            Silver
+                        </td>
+                        <td class="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td class="px-6 py-4">
+                            $2999
+                        </td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+   </div>
 </template>
 
 <script>
-import LargeTitle from '@/components/sub_components/LargeTitle.vue'
+import SearchButton from '@/components/Appointments/sub_components/SearchButton.vue'
 
 export default {
     name: 'AccountsPage',
     components: {
-        // LargeTitle,
+        SearchButton
     },
     computed: {
         pageTitle(){
